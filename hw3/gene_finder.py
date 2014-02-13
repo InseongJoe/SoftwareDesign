a883982d7f14dc76e8e790d52090b95d82800ac7
Inseong Joe <inseong.joe@students.olin.edu>
2014-02-13 00:49:32 -0500

    homework 3 turnin

--- hw3/gene_finder.py
+++ hw3/gene_finder.py
@@ -7,6 +7,7 @@ Created on Sun Feb  2 11:24:42 2014
 
 # you may find it useful to import these variables (although you are not required to use them)
 from amino_acids import aa, codons
+from random import shuffle
 
 def collapse(L):
     """ Converts a list of strings to a string by concatenating all elements of the list """
@@ -27,12 +28,22 @@ def coding_strand_to_AA(dna):
     """
     
     # YOUR IMPLEMENTATION HERE
-
+    l = []
+    for x in range(0,len(dna)/3*3,3):
+        cod = dna[x:x+3]
+        l.append(get_amino_acid_for_codon(cod))
+    return collapse(l)
+    
+def get_amino_acid_for_codon(c):
+    for i in range(len(codons)):
+        if c in codons[i]:
+            return aa[i]
+       
 def coding_strand_to_AA_unit_tests():
-    """ Unit tests for the coding_strand_to_AA function """
-        
+    """ Unit tests for the coding_strand_to_AA function """        
     # YOUR IMPLEMENTATION HERE
-
+    print 'input :'+str(['ATGTGTCGTTAG']), 'expected output : MCR|', 'actual output :' + coding_strand_to_AA('ATGTGTCGTTAG')
+    print 'input :'+str(['ATGCCCTGTGAATT']), 'expected output : MPCE', 'actual output :' + coding_strand_to_AA('ATGCCCTGTGAATT')
 def get_reverse_complement(dna):
     """ Computes the reverse complementary sequence of DNA for the specfied DNA
         sequence
@@ -42,11 +53,27 @@ def get_reverse_complement(dna):
     """
     
     # YOUR IMPLEMENTATION HERE
+    d = list(dna)
+    for i in range(0,len(d)):
+        if d[i] == 'A':
+            d[i] = 'T'
+        elif d[i] == 'T':
+            d[i] = 'A'
+        elif d[i] == 'C':
+            d[i] = 'G'
+        elif d[i] == 'G':
+            d[i] = 'C'
+    n = d[::-1]
+    return collapse(n)
     
+    
+        
 def get_reverse_complement_unit_tests():
     """ Unit tests for the get_complement function """
         
-    # YOUR IMPLEMENTATION HERE    
+    # YOUR IMPLEMENTATION HERE
+    print 'input :'+str(['ATGTGTCGTTAG']), 'expected output : CTAACGACACAT', 'actual output :' + get_reverse_complement('ATGTGTCGTTAG')
+    print 'input :' + str(['ATCCGT']), 'expected output : ACGGAT', 'actual output :' + get_reverse_complement('ATCCGT')
 
 def rest_of_ORF(dna):
     """ Takes a DNA sequence that is assumed to begin with a start codon and returns
@@ -56,14 +83,21 @@ def rest_of_ORF(dna):
         dna: a DNA sequence
         returns: the open reading frame represented as a string
     """
-    
     # YOUR IMPLEMENTATION HERE
+    for i in range(0,len(dna),3):
+        cod = dna[i:i+3]
+        if cod == ('TAG' or 'TAA' or 'TGA'):
+            return dna[0:i]
+    return dna
+        
 
 def rest_of_ORF_unit_tests():
     """ Unit tests for the rest_of_ORF function """
         
     # YOUR IMPLEMENTATION HERE
-        
+    print 'input :'+str(['ATGTGTCGTTAG']), 'expected output : ATGTGTCGT', 'actual output :' + rest_of_ORF('ATGTGTCGTTAG')
+    print 'input :' + str(['ATGTCTATGCCCTAA']), 'expected output : ATGTCTATGCCCTAA','actual output :' + rest_of_ORF('ATGTCTATGCCCTAA')
+    
 def find_all_ORFs_oneframe(dna):
     """ Finds all non-nested open reading frames in the given DNA sequence and returns
         them as a list.  This function should only find ORFs that are in the default
@@ -76,12 +110,31 @@ def find_all_ORFs_oneframe(dna):
     """
      
     # YOUR IMPLEMENTATION HERE        
-     
+    # GCA TGA ATG TAG
+    # ATG CGA ATG TAG CAT CAA A
+    val = []
+    while len(dna)>2:
+        for x in range(0,len(dna),3):
+            if dna[x:x+3] == 'ATG':
+                k = dna[x:]
+                sth = rest_of_ORF(k)
+                #print dna, k, sth
+                val.append(sth)
+                dna = dna[x + len(sth)+3:]
+                break
+            else:
+                dna = dna[3:]
+                break
+    return val
+              
+        
 def find_all_ORFs_oneframe_unit_tests():
     """ Unit tests for the find_all_ORFs_oneframe function """
 
     # YOUR IMPLEMENTATION HERE
-
+    print 'input :'+str(['ATGCGATAGTCTATGCCC']), "expected output :['ATGCGA','ATGCCC']" , 'actual output :' + str(find_all_ORFs_oneframe('ATGCGATAGTCTATGCCC'))
+    print 'input :'+str(['TCTATGCCCTA']), "expected output : ['ATGCCCTA']",'actual output :'+str(find_all_ORFs_oneframe('TCTATGCCCTA'))    
+    
 def find_all_ORFs(dna):
     """ Finds all non-nested open reading frames in the given DNA sequence in all 3
         possible frames and returns them as a list.  By non-nested we mean that if an
@@ -93,12 +146,28 @@ def find_all_ORFs(dna):
     """
      
     # YOUR IMPLEMENTATION HERE
+    val = []
+    for j in range(0,3):
+        dnanew = dna[j:]
+        #print dnanew
+        # ATG CGA ATG TAG CAT CAA A
+        things = find_all_ORFs_oneframe(dnanew)
+        #print dnanew, things
+        val += things
+    return val
+"""
+ATG CAT GAA TGT AG
+TGC ATG AAT GTA G
+GCA TGA ATG TAG
+"""
 
 def find_all_ORFs_unit_tests():
     """ Unit tests for the find_all_ORFs function """
         
     # YOUR IMPLEMENTATION HERE
-
+    print 'input :'+str(['ATGCCGAATGTACC']), "expected output: ['ATGCCGAATGTACC', 'ATGTACC']",'actual output :'+str(find_all_ORFs('ATGCCGAATGTACC'))
+    print 'input :'+str(['TACGATGTACCG']), "expected output: ['ATGTACCG']",'actual output :'+str(find_all_ORFs('TACGATGTACCG'))
+    
 def find_all_ORFs_both_strands(dna):
     """ Finds all non-nested open reading frames in the given DNA sequence on both
         strands.
@@ -108,23 +177,37 @@ def find_all_ORFs_both_strands(dna):
     """
      
     # YOUR IMPLEMENTATION HERE
-
+    val = []
+    k = find_all_ORFs(dna)
+    val = k
+    revdna = get_reverse_complement(dna)
+    l = find_all_ORFs(revdna)
+    val += l
+    return val
+    
 def find_all_ORFs_both_strands_unit_tests():
     """ Unit tests for the find_all_ORFs_both_strands function """
 
     # YOUR IMPLEMENTATION HERE
-
+    print 'input :'+str('ATGCTAAGGTAGTTGTGATAA'), "expected output: ['ATGCTAAGG']",'actual output :'+str(find_all_ORFs_both_strands('ATGCTAAGGTAGTTGTGATAA'))
+    print 'input :'+str('TAGCGATGTTCTGAGGTAA'), "expected output: ['ATGTTCTGAGGTAA']",'actual output :'+str(find_all_ORFs_both_strands('TAGCGATGTTCTGAGGTAA'))
+    print 'input :' + str('ATGCGAATGTAGCATCAAA'),"expected output: ['ATGCGAATG', 'ATGCTACATTCGCAT']",'actual output :'+str(find_all_ORFs_both_strands('ATGCGAATGTAGCATCAAA'))
 def longest_ORF(dna):
     """ Finds the longest ORF on both strands of the specified DNA and returns it
         as a string"""
 
     # YOUR IMPLEMENTATION HERE
+    k = find_all_ORFs_both_strands(dna)
+    return max(k, key=len)
+    
 
 def longest_ORF_unit_tests():
     """ Unit tests for the longest_ORF function """
 
     # YOUR IMPLEMENTATION HERE
-
+    print 'input :'+str('ATGCCGTTGTACAGTTAGG'), "expected output: ATGCCGTTGTACAGT",'actual output :'+str(longest_ORF('ATGCCGTTGTACAGTTAGG'))
+    print 'input :'+str('TGCAATATGCGATTTTAG'), "expected output: ATGCGATTT",'actual output :'+str(longest_ORF('TGCAATATGCGATTTTAG'))
+    
 def longest_ORF_noncoding(dna, num_trials):
     """ Computes the maximum length of the longest ORF over num_trials shuffles
         of the specfied DNA sequence
@@ -134,6 +217,12 @@ def longest_ORF_noncoding(dna, num_trials):
         returns: the maximum length longest ORF """
 
     # YOUR IMPLEMENTATION HERE
+    a = list(dna)
+    shuffle(a)
+    b = collapse(a)
+    c = longest_ORF(b)
+    return c
+
 
 def gene_finder(dna, threshold):
     """ Returns the amino acid sequences coded by all genes that have an ORF
@@ -147,3 +236,32 @@ def gene_finder(dna, threshold):
     """
 
     # YOUR IMPLEMENTATION HERE
+    val = []
+    a = find_all_ORFs_both_strands(dna)
+    for i in a:
+        if len(i) >= threshold:
+            a = coding_strand_to_AA(i)
+            val.append(a)
+        return val
+            
+'''
+I used gene_finder with the salmonella dna data and a threshold of about 800 because
+after I did a couple of random trials I got numbers from about 600 to 1200. So, I decided
+to go with 800 as the threshold.
+
+['MGIFASAGCGKTMLMHMLIEQTEADVFVIGLIGERGREVTEFVDMLRASHKKEKCVLVFATSDFPSVDRCNAAQ
+LATTVAEYFRDQGKRVVLFIDSMTRYARALRDVALASGERPARRGYPASVFDNLPRLLERPGATSEGSITAFYTVL
+LESEEEADPMADEIRSILDGHLYLSRKLAGQGHYPAIDVLKSVSRVFGQVTTPTHAEQASAVRKLMTRLEELQLFI
+DLGEYRPGENIDNDRAMQMRDSLKAWLCQPVAQYSSFDDTLSGMNAFADQN|SIAAALYGISFTV|VDITSLSG|
+GPRAAGRGGGDP|TNSGSEIVIRYAACRKQTAQS|GNLYVIT|AVYCSPADKRFRTPDYTNSGKTERAGKEKGRV
+SEKK|ILVAQRRELSTLDNPSEKILYPARDTAGRGRVRGDNLMGDVSAVSSSGNILLPQQDEVGGLSEALKKAVE
+KHKTEYSGDKKDRDYGDAFVMHKETALPLLLAAWRHGAPAKSEHHNGNVSGLHHNGKSELRIAEKLLKVTAEKSV
+GLISAEAKVDKSAALLSSKNRPLESVSGKKLSADLKAVESVSEVTDNATGISDDNIKALPGDNKAIAGEGVRKEG
+APLARDVAPARMAAANTGKPEDKDHKKVKDVSQLPLQPTTIADLSQLTGGDEKMPLAAQSKPMMTIFPTADGVKG
+EDSSLTYRFQRWGNDYSVNIQARQAGEFSLIPSNTQVEHRLHDQWQNGNPQRWHLTRDDQQNPQQQQHRQQSGEE
+DDA|CHCV|DRLIVANGYWRKPRQNASAMAGKRRWNIRRDRECGFG|AMQKNGGRPGLNLGTGLSMSLPLWLGRR
+FLLALSTWSFPGLLQQSDRLSCPCRICPVGVYA']
+
+This is what I got from gene_finder.
+I put this in BLAST.
+'''
